5bfac04f019f25e320a2e40ba7f2cc08241333ec
nicolas moscoso <2nicolasmoscoso4@gmail.com>
2023-11-07 12:09:08 -0500

    nuevos cambios agregados

---
+++ main.py
@@ -0,0 +1,46 @@
+numero_entero = 42
+numero_flotante = 3.14
+letra = 'A'
+es_verdadero = True
+print("numero_entero", numero_entero)
+print("numero_foltante", numero_flotante)
+print("letra" , letra)
+print("es_verdadero", es_verdadero, "m")
+
+#se definen cuatro variables de tipo primitivo: "entero"(int), "decimal"(float), "booleano"(bool) y "cadena"(str)
+#las variables que no son cadena se concatetan junto a la variable cadena el resultado se almacena dentro de la variable "resultado"
+entero = 24
+decimal = 8.11
+booleano = True
+cadena = "Hola"
+resultado = str(entero) + " " + str(decimal) + " " + str(booleano) + " " + cadena
+print(resultado)
+
+#enteros (int) un numero que no tiene decimales, Pueden ser negativos, positivos o cero se realizan operacion entre ellos
+
+numeros_int = 1,2,3,4,5,6,7,8,9,10
+print(numeros_int)
+
+primer_numero = 27
+segundo_numero = 63
+
+#suma
+print(primer_numero + segundo_numero)
+
+#resta
+print(primer_numero-segundo_numero)
+
+#multiplicacion
+print(primer_numero*segundo_numero)
+
+#se genera un limite cuando en la operacion se generan numeros decimales 
+
+#division 
+print(primer_numero/segundo_numero)
+
+#division sin decimales
+print(primer_numero//segundo_numero)
+
+
+#flotantes (float) numeros que si tienen decimales
+numeros_float = 1.1, 1.2 , 3.4
